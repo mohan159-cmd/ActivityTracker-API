0c7c2bf142e52cd231136aa023cb9063bcbd7735
mohan159-cmd <mohannimmala159@gmail.com>
2024-11-13 23:04:19 +0530

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,12 +7,11 @@ app.use(express.json()); // To parse JSON requests
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Welcome to the Movie and Employee Management System!');
+  res.send('Welcome to the Movie Collection Management System!');
 });
 
 app.use('/v1/', apiRouters);
   
 app.listen(8080, () => {
   console.log('Server is running on port 8080');
-  console.log('Swagger UI available at http://localhost:8080/api-docs');
 });
