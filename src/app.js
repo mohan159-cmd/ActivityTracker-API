5962b03bea444e1014d0202d1ef58eeb521e4885
Mohan <mohan.nimmala@bluehublabs.net>
2024-12-10 18:59:02 +0530

    Vercel Json Updation

--- src/app.js
+++ src/app.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const app = express();
-
 const apiRouters = require('./routes');
 
 app.use(express.json()); // To parse JSON requests
 
-// Routes
+// Root route
 app.get('/', (req, res) => {
   res.send('Welcome to the Tracker API!');
 });
 
+// API routes
 app.use('/v1/', apiRouters);
-  
+
+// Start the server
 app.listen(8080, () => {
   console.log('Server is running on port 8080');
 });
