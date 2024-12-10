1f54b8968e5cf0220c7423fecd30204956d2d948
mohan159-cmd <mohannimmala159@gmail.com>
2024-12-10 20:24:34 +0530

    App.js Changes to accomadate verel

--- src/app.js
+++ src/app.js
@@ -2,17 +2,24 @@ const express = require('express');
 const app = express();
 const apiRouters = require('./routes');
 
-app.use(express.json()); // To parse JSON requests
+// Middleware to parse JSON requests
+app.use(express.json());
 
-// Root route
+// Test Route
 app.get('/', (req, res) => {
   res.send('Welcome to the Tracker API!');
 });
 
-// API routes
-app.use('/v1/', apiRouters);
+// Your API routes
+app.use('/v1', apiRouters);
 
-// Start the server
+/* // Start the server
 app.listen(8080, () => {
   console.log('Server is running on port 8080');
-});
+}); */
+
+
+// Export app as a handler for Vercel
+module.exports = (req, res) => {
+  app(req, res);  // Use the Express app to handle requests
+};
