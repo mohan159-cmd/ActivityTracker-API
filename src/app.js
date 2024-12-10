651f835524b1a217f7e3d389fec666385df57e58
mohan159-cmd <mohannimmala159@gmail.com>
2024-12-10 20:35:21 +0530

    Deployement Test 1

--- src/app.js
+++ src/app.js
@@ -1,25 +1,3 @@
-const express = require('express');
-const app = express();
-const apiRouters = require('./routes');
-
-// Middleware to parse JSON requests
-app.use(express.json());
-
-// Test Route
-app.get('/', (req, res) => {
-  res.send('Welcome to the Tracker API!');
-});
-
-// Your API routes
-app.use('/v1', apiRouters);
-
-/* // Start the server
-app.listen(8080, () => {
-  console.log('Server is running on port 8080');
-}); */
-
-
-// Export app as a handler for Vercel
 module.exports = (req, res) => {
-  app(req, res);  // Use the Express app to handle requests
+  res.status(200).json({ message: 'Serverless Function is working!' });
 };
