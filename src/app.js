5c5f86de9db2d8b0bda9a5b53dce195937e7adaa
Mohan <mohan.nimmala@bluehublabs.net>
2024-12-23 15:46:46 +0530

    Update error response message and uncomment server start code in app.js

--- src/app.js
+++ src/app.js
@@ -16,15 +16,16 @@ app.use('/v1', apiRouters);
 // Error-handling middleware
 app.use((err, req, res, next) => {
   console.error('Error:', err.stack || err);
-  res.status(500).json({ message: 'Internal Server Error' });
+  res.status(500).json({ message: 'Internal Server Error 159' });
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080; // Default to 3000 if no PORT env variable
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 /* // Start the server
 app.listen(8080, () => {
   console.log('Server is running on port 8080');
-});  */
-
-// Export as handler for web app
-module.exports = (req, res) => {
-  app(req, res);
-};
+});  */ 
