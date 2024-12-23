e39c9658cd36de1f2f1f5f230ebf1f7e439e2ffe
Mohan <mohan.nimmala@bluehublabs.net>
2024-12-23 15:32:49 +0530

    Refactor application structure: move app.js to src directory, update package.json, and remove old app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const apiRouters = require('./src/routes');
+const apiRouters = require('./routes');
 
 // Middleware to parse JSON requests
 app.use(express.json());
