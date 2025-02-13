4b32d8c2a5c89f21bb968200f1ed7035bd901517
Mohan <mohan.nimmala@bluehublabs.net>
2025-02-13 20:12:26 +0530

    Comment out server port configuration and listen function

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 8080;
+/* const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-});
+}); */
