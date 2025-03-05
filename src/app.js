6ed2218add1d140f6df25f433d7b18fedba4d34a
mohan159-cmd <mohannimmala159@gmail.com>
2025-03-05 10:10:53 +0530

    Disabling Routes

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
 });
 
 // API routes
-app.use('/v1', apiRouters);
+/* app.use('/v1', apiRouters); */
 
 // Error-handling middleware
 app.use((err, req, res, next) => {
@@ -24,11 +24,11 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Internal Server Error 159' });
 });
 
-/* // Start the server
+// Start the server
 const PORT = process.env.PORT || 8080;
 if (!PORT) {
   throw new Error('PORT is not defined');
 }
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-}); */
+});
